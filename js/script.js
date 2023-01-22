fc302ac1d977b58ddc4b2b633ef000756fd310a0
kapi1023 <57112274+kapi1023@users.noreply.github.com>
2023-01-22 16:08:50 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-
 const hamburgerButton = document.getElementById("menu-btn")
 const navbar = document.querySelector(".navbar")
 hamburgerButton.addEventListener("click",() =>{
     console.log("klik")
     navbar.classList.toggle('active')
 })
+
+
+
+
+var show = true;
+document.getElementById("seeAll").addEventListener("click", function(){
+  var rows = document.getElementById("postDataTable").rows;
+  if (show) {
+    for (var i = 3; i < rows.length; i++) {
+      rows[i].style.display = "table-row";
+    }
+    show = false;
+  } else {
+    for (var i = 3; i < rows.length; i++) {
+      rows[i].style.display = "none";
+    }
+    show = true;
+  }
+});
